75293d2ddbb96fdb0c4d683530bd17e23d62b03c
letelete <brunokawka@gmail.com>
2024-11-03 23:32:19 +0100

    [MVP] Polyfill window.crypto when run in node environment

--- src/core/scene/key-generator.ts
+++ src/core/scene/key-generator.ts
@@ -1,8 +1,15 @@
+const randomUUID = () => {
+  if (typeof crypto.randomUUID !== 'function') {
+    return require('crypto').randomUUID() as string;
+  }
+  return crypto.randomUUID() as string;
+};
+
 type KeyGenerator = Generator<string, string, string>;
 
 function* createKeyGenerator(): KeyGenerator {
   while (true) {
-    yield crypto.randomUUID() as string;
+    yield randomUUID();
   }
 }
 
